6b962fbc3c58c9880a31e4544decfa753d161bce
Tanya Bhadouria <78158802+tanyabhadouria@users.noreply.github.com>
2024-11-05 22:25:04 +0530

    Add files via upload

---
+++ Bakery/script.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "240px";
+    document.getElementById("main").style.marginLeft = "203px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "";
+  }
+
+
+  
